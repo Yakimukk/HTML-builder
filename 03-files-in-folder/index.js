370a099de124d7fb04e49d0f76944f2b53b38f48
Denis V <denisv39@mail.ru>
2022-05-23 23:53:43 +0200

    feat: add solution for 3 task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+const folder = path.join(__dirname, 'secret-folder')
+
+fs.readdir(folder, {withFileTypes: true}, (err, files) => {
+    for (const file of files) {
+        if (file.isFile()) {
+            const folderFile = path.join(folder, file.name)
+            const extension = path.extname(folderFile)
+            const name = file.name.split('.')[0]
+            fs.stat(folderFile, (err, stats) => {
+                const size = `${(stats.size/1024).toFixed(3)}kb`
+            console.log(`${name} - ${extension} - ${size}`)
+            })
+        }
+    }
+})
